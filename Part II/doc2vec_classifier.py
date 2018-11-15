cecb28b4b353316de47ae0b80ca31642985cf09d
macro206 <mpozzi@hotmail.co.uk>
2018-11-15 20:47:51 +0000

    Doc2vec classifier is now working

--- Part II/doc2vec_classifier.py
+++ Part II/doc2vec_classifier.py
@@ -1,15 +1,52 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.utils import tokenize
 from review_loader import getFeaturesForAllReviews
 import svmlight
+import os
+import string
 
-def trainModel(trainingData):
-    taggedDocuments = [TaggedDocument(doc[2], [doc[1]]) for i,doc in enumerate(trainingData)]
+### DOC2VEC MODEL TRAINING ###
+
+def importFolder(folder):
+    folderList = sorted(list(filter(lambda s: s.endswith(".txt"), os.listdir(folder))))
+
+    reviews = []
+
+    for fileName in folderList:
+        with open(folder + fileName, 'r') as file:
+            reviews.append(list(tokenize(file.read())))
+
+    return reviews
+
+def importData():
+    # TODO: CHANGE THIS WHEN RUNNING ON ANOTHER MACHINE
+    corpusRoot = "/Users/Matteo/Desktop/aclImdb/"
+
+    paths = ["train/neg/",
+             "train/pos/",
+             "train/unsup/",
+             "test/neg/",
+             "test/pos/"]
+
+    docs = []
+    for p in paths:
+        docs.extend(importFolder(corpusRoot+p))
+
+    return docs
+
+def generateDoc2VecModel():
+    trainingData = importData()
+
+    print("Data imported")
+
+    taggedDocuments = [TaggedDocument(doc, [i+1]) for i,doc in enumerate(trainingData)]
     model = Doc2Vec(taggedDocuments, dm=1, vector_size=100, max_epochs=10)
 
-    return model
+    model.save("/Users/Matteo/Desktop/doc2vec_model")
+
 
 def performDoc2VecClassification(trainingData, testData):
-    doc2vecModel = trainModel(trainingData)
+    doc2vecModel = Doc2Vec.load("/Users/Matteo/Desktop/doc2vec_model")
 
     trainingFeatureVectors = [(1 if doc[0] == 'POS' else -1, doc2vecModel.infer_vector(doc[2])) for doc in trainingData]
     testFeatureVectors = [(0, doc2vecModel.infer_vector(doc[2])) for doc in testData]
@@ -32,7 +69,6 @@ def performDoc2VecClassification(trainingData, testData):
         i += 1
 
     print(float(nCorrect)/float(len(judgements)))
-
 
 
 
